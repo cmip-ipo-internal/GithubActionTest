7ab17abedba1b4a5612814b05dce0d60a76a3d07
Dan Ellis <daniel.ellis.research@gmail.com>
2023-10-18 20:51:09 +0100

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -6,7 +6,21 @@ print(body)
 try:
   os.mkdir(sys.argv[1])
   os.system(f'touch {sys.argv[1]+"test"}')
-  print('yay dsjfdskljklj dklsjf ldks fdjslkj fdskljfd slkjf dslkj fdsklj fdsj l')
+  print(sys.argv[1]+'yay dsjfdskljklj dklsjf ldks fdjslkj fdskljfd slkjf dslkj fdsklj fdsj l')
 
 except Exception as err: 
   print('FAILED',err)
+
+
+'''
+env 
+GITHUB_WORKSPACE = directory location
+same as 'PWD': '/home/runner/work/GithubActionTest/GithubActionTest'
+
+'GITHUB_TRIGGERING_ACTOR': 'wolfiex',
+
+
+'GITHUB_REPOSITORY': 'cmip-ipo-internal/GithubActionTest'
+
+'''
+
