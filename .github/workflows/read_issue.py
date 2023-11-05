9ac06893d27008a233fe7a0021b35b85519288fb
Dan Ellis <daniel.ellis.research@gmail.com>
2023-11-05 20:38:32 +0000

    Update read_issue.py

--- .github/workflows/read_issue.py
+++ .github/workflows/read_issue.py
@@ -1,32 +1,33 @@
 import subprocess
 import json
 
-def get_issue_body():
+def get_issue_fields():
     try:
-        # Run jq command to extract issue body from GITHUB_EVENT_PATH
-        result = subprocess.run(['jq', '-r', '.issue.body', '$GITHUB_EVENT_PATH'], capture_output=True, text=True)
+        # Run jq command to extract all fields of the issue from GITHUB_EVENT_PATH
+        result = subprocess.run(['jq', '.issue', '$GITHUB_EVENT_PATH'], capture_output=True, text=True)
+        print(f'Command output: {result.stdout}')  # Debugging line
         # Parse the output as JSON
-        issue_body = json.loads(result.stdout.strip())
-        return issue_body
+        issue_fields = json.loads(result.stdout.strip())
+        return issue_fields
     except Exception as e:
         print(f"Error: {e}")
         return None
 
-def write_to_file(issue_body, filename):
+def write_to_file(data, filename):
     try:
-        # Write issue body to the specified file
+        # Write data to the specified file in JSON format
         with open(filename, 'w') as file:
-            file.write(issue_body)
-        print(f"Issue body written to {filename}")
+            json.dump(data, file, indent=4)
+        print(f"Issue fields written to {filename}")
     except Exception as e:
         print(f"Error: {e}")
 
 # Example usage
 if __name__ == "__main__":
-    issue_body = get_issue_body()
-    if issue_body:
-        # Specify the filename where you want to write the issue body
-        filename = "issue_body.txt"
-        write_to_file(issue_body, filename)
+    issue_fields = get_issue_fields()
+    if issue_fields:
+        # Specify the filename where you want to write the issue fields as JSON
+        filename = "issue_fields.json"
+        write_to_file(issue_fields, filename)
     else:
-        print("Failed to retrieve issue body.")
+        print("Failed to retrieve issue fields.")
