b577be85bb50adb9f393c82ae13ae16b6b0515fe
Dan Ellis <daniel.ellis.research@gmail.com>
2023-11-05 22:53:38 +0000

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,6 +1,10 @@
 import os 
 import sys
 import requests
+import re
+import json
+from collections import OrderedDict
+
 
 URL_TEMPLATE = 'https://api.ror.org/organizations/{}'
 
@@ -47,36 +51,110 @@ def parse_ror_data(ror_data):
         return None
 
 
+def get_variables(input_string):
 
-body = os.environ.keys()
-print(body)
-print(sys.argv)
-
-with open('../ouput.txt','w') as f:
-    f.write(str(body))
-    f.write('\n\n')
-    f.write(str(sys.argv))
-    f.write('\n\n')
-
-
-try:
-  os.mkdir(sys.argv[1])
-  os.system(f'touch {sys.argv[1]+"test"}')
-  print(sys.argv[1]+'yay')
-
-except Exception as err: 
-  print('FAILED',err)
+    # Regular expression pattern to match variables denoted between **
+    pattern = r"\*\*(.*?)\*\*:\s*\"(.*?)\""
+    
+    # Extract variables and their contents using regular expression
+    variables = dict(re.findall(pattern, input_string))
+    
+    # Print extracted variables
+    print(variables)
 
 
-'''
-env 
-GITHUB_WORKSPACE = directory location
-same as 'PWD': '/home/runner/work/GithubActionTest/GithubActionTest'
 
-'GITHUB_TRIGGERING_ACTOR': 'wolfiex',
+def get_issue_fields():
+    try:
+        # Get the path to the GitHub event payload file
+        event_path = os.getenv('GITHUB_EVENT_PATH')
 
-
-'GITHUB_REPOSITORY': 'cmip-ipo-internal/GithubActionTest'
+        # Read the contents of the event payload file
+        with open(event_path, 'r') as file:
+            event_data = json.load(file)
+            # Extract the entire .issue object
+            issue_fields = event_data.get('issue', {})
+            return issue_fields
+    except Exception as e:
+        print(f"FAILED to read the Issue contents: {e}")
+        return None
 
 '''
+Simialrity algorithm
+'''
+def levenshtein_distance(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+    
+    if len(s2) == 0:
+        return len(s1)
+    
+    previous_row = range(len(s2) + 1)
+    for i, char1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, char2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (char1 != char2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return previous_row[-1]
 
+def similarity_score(s1, s2):
+    max_length = max(len(s1), len(s2))
+    if max_length == 0:
+        return 1.0
+    
+    distance = levenshtein_distance(s1, s2)
+    similarity = 1.0 - (distance / max_length)
+    return similarity
+
+
+
+
+
+if __name__ == '__main__':
+    body = issue_fields.body
+    varaibles = get_variables(body)
+    # {'Issuer': 'dan', 'Name': 'York', 'ROR link': 'https://ror.org/04m01e293'} 'ACRONYM'
+    print(variables)
+
+    ror_id = variables['ROR'].split('/')[-1]
+    ror_data = get_ror_data(ror_id)
+    ror = parse_ror_data(ror_data)
+
+    # checks
+    name = ror['indentifiers']['institution_name']
+    threshold = 0.6
+    if similarity_score(name, variables['Name']) < threshold:
+        sys.exit(f"FAILED: The names are not similar enough. please confirm. \n Name provided: {variables['Name']} \n ROR given name: {name}")
+
+    filename = '../../institutions.json'
+    data = json.load(open(filename, 'r'))
+
+    acronym = variables['Acronym']
+    if acronym in data:
+        sys.exit(f'FAILED: {acronym} already exists')
+
+    rors = [i['indentifiers']['ror'] for i in data.values()]
+    if ror_id in rors:
+        where = list(dict.keys())[rors.index(ror_id)]
+        sys.exit(f'FAILED: {ror_id} already exists in the list under key "{where}"')
+
+
+    # final check that acronym already exists in ROR?
+
+    data[acronym] = ror
+
+    # Sort the dictionary by keys alphabetically and maintain order
+    data = OrderedDict(sorted(data.items(), key=lambda item: item[0]))
+
+    with open(filename, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+
+
+    
+    
